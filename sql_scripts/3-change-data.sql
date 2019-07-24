eec9abe1e1ae785eb1d2e697593ce9fd269927c6
gabriel passos <gabriel.passos@agibank.com.br>
2019-07-24 15:26:00 -0300

    :recycle: ajuste no script

--- sql_scripts/3-change-data.sql
+++ sql_scripts/3-change-data.sql
@@ -9,3 +9,9 @@ WHERE TAX_ID = '12494695465';
 
 -- check columns that are listen
 select * from cdc.captured_columns;
+
+select * from cdc.fn_cdc_get_all_changes_change_data_capture_test_PERSON(
+  0x0000002400000C880043,
+  CAST(10000000000 AS BINARY(4)),
+  ''
+)
